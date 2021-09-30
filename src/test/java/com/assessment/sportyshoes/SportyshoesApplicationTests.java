142f479810d3b820de8a76413b44ac84912cdc55
Piyush Pranjal (pipranja) <pipranja@cisco.com>
2021-09-30 16:03:50 +0530

    initial commit

---
+++ src/test/java/com/assessment/sportyshoes/SportyshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.sportyshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
